5886488b62e00a7e234dd5421ceba4138d02c047
JoseLuisColcha <jose.colcha@epn.edu.ec>
2021-12-06 13:49:01 -0500

    home

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer bg-dark text-center text-white">
     <div class="container p-3">
-        Todos los derechos reservados @GRUPO3
+        Todos los derechos reservados @GRUPO3.
     </div>
 </footer>
